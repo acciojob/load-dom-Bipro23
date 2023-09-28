0ad1ccb5848f00d4e09ba32c0c4813748e0b0fee
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-28 19:46:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 //your JS code here. If required.
-//your JS code here. If required.
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-  // This code will run when the DOM is fully loaded
-  const paragraph = document.createElement("p"); // Create a new paragraph element
-  paragraph.textContent = "DOM load success"; // Set its text content
 
-  // Append the paragraph to the body of the document
-  document.body.appendChild(paragraph);
-});
+document.addEventListener("DOMContentLoaded",(e)=>{
+	
+	document.body.textContent="DOM load success"
+	
+})
